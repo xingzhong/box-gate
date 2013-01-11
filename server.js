ac65630ea5f7dbc4ff39ef44ddfd25522bd26168
Xingzhong <xu.xeno@gmail.com>
2013-01-11 18:03:14 -0500

    add data structure

--- server.js
+++ server.js
@@ -1,41 +1,50 @@
 var express = require('express')
-	, app = express()
-  	, server = require('http').createServer(app)
-  	, io = require('socket.io').listen(server);
+	   , app = express()
+  	 , server = require('http').createServer(app)
+  	 , io = require('socket.io').listen(server);
   
 app.use(express.static(__dirname + "/static"))
 
 var mongoose = require('mongoose');
 mongoose.connect('localhost', 'test');
-var schema = mongoose.Schema({ timeIn: Date, timeOut: Date });
-var Time = mongoose.model('Time', schema);
+var schema = mongoose.Schema({ 
+  timeIn: Date,
+  uid: Number,
+  did: Number });
+var Record = mongoose.model('Record', schema);
 
-server.listen(8000);
+server.listen(8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
+  
   socket.on('log', function (data) {
-	time = new Time({
-		timeIn: data.timeIn,
-		timeOut: new Date().getTime()
-	});
-	time.save(function (err) {
-	  if (!err) 
-	  	console.log('save');
-	});
-
-	
+  	var record = new Record({
+    		timeIn: data.timeIn,
+    		uid: data.uid,
+        did: data.did,
+    	});
+  	record.save(function (err) {
+  	  if (!err) 
+  	  	console.log('save');
+  	});
   });
+
+  socket.on("delRec", function(id){
+    console.log(id);
+    Record.findByIdAndRemove(id).exec();
+  });
+
   socket.on('listready', function (data) {
-      Time.find()
+      Record.find()
   		.sort({timeIn: -1})
   		.limit(20)
-  		.exec(function(err, times) {
+  		.exec(function(err, records) {
       	if (!err) {
-      		socket.emit('list', times);
+      		socket.emit('list', records);
      	}});
   });
 });
