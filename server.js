0c1a8915c516b6e0c71df42b9106014de421368a
Xingzhong <xu.xeno@gmail.com>
2013-01-13 16:39:38 -0500

    stress test mongodb

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/*
+  the interface MongoDB through WebSocket 
+*/
+
 var express = require('express')
 	   , app = express()
   	 , server = require('http').createServer(app)
@@ -8,9 +12,17 @@ app.use(express.static(__dirname + "/static"))
 var mongoose = require('mongoose');
 mongoose.connect('localhost', 'test');
 var schema = mongoose.Schema({ 
-  timeIn: Date,
-  uid: Number,
-  did: Number });
+  Datetime: Date,
+  Depth: Number,
+  Eqid: String,
+  Lat: Number,
+  Lon: Number,
+  Magnitude: Number, 
+  NST: Number,
+  Region: String,
+  Src: String,
+  Version: String,
+});
 var Record = mongoose.model('Record', schema);
 
 server.listen(8080);
@@ -22,10 +34,18 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (socket) {
   
   socket.on('log', function (data) {
+    console.log(data);
   	var record = new Record({
-    		timeIn: data.timeIn,
-    		uid: data.uid,
-        did: data.did,
+    		Datetime: data.Datetime,
+    		Depth: data.Depth,
+        Eqid: data.Eqid,
+        Lat: data.Lat,
+        Lon: data.Lon,
+        Magnitude: data.Magnitude,
+        NST: data.NST,
+        Region: data.Region,
+        Src: data.Src,
+        Version: data.Version,
     	});
   	record.save(function (err) {
   	  if (!err) 
@@ -38,6 +58,16 @@ io.sockets.on('connection', function (socket) {
     Record.findByIdAndRemove(id).exec();
   });
 
+  socket.on("cleanDB", function(){
+    Record.find().exec(function(err, records){
+      if(!err){
+        records.forEach(function(d){
+          d.remove();
+        })
+      }
+    });
+  });
+
   socket.on('listready', function (data) {
       Record.find()
   		.sort({timeIn: -1})
@@ -46,6 +76,14 @@ io.sockets.on('connection', function (socket) {
       	if (!err) {
       		socket.emit('list', records);
      	}});
+  });
+
+  socket.on("count", function(data) {
+    Record.count({}, function(err, count) {
+      if (!err) {
+        socket.emit("countReady", count);
+      }
+    });
   });
 });
 
