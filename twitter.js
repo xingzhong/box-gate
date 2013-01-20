b1e7100a20d5fa87ace359ba9518a1f5bf542000
Xingzhong <xu.xeno@gmail.com>
2013-01-19 23:02:08 -0500

    change port

--- twitter.js
+++ twitter.js
@@ -1,6 +1,8 @@
 var twitter = require('ntwitter');
-var port = process.env.PORT || 5000;
-var io = require('socket.io').listen(port);
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
 	socket.on('disconnect', function() {
